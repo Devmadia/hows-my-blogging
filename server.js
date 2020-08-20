56431d043e66bebf7f281f76ecf8afdc7ad2357e
Devmadia <agathaphan@icloud.com>
2020-08-20 18:02:07 -0500

    initializing folders for setup of server architectural structure

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// handlebars template engine
+const exphbs = require('express-handlebars');
+
+// sequelizestore import
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// handlebars template engine
+const hbs = exphbs.create({ helpers });
+
+// handlebars template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+// turn on routes
+app.use(routes);
+
+// drop the tables so the application can re-create them and implement the associations
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on POT ${PORT}'));
+});
