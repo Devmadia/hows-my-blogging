9a8e3a234541334d087e3799f8e1b9c9eed41fa6
Devmadia <agathaphan@icloud.com>
2020-08-22 19:16:53 -0500

    added API routes and use route for returning HTTP 404 error if connection not found

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
 
+// API routes
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
+router.use((req, res) => {
+	res.status(404).end();
+});
 
 module.exports = router;
