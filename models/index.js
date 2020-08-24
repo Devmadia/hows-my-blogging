8ae8ceaa67c707f338d872131abfadb2616e3773
Devmadia <agathaphan@icloud.com>
2020-08-24 17:49:15 -0500

    updated model relationships

--- models/index.js
+++ models/index.js
@@ -7,19 +7,27 @@ and never many users. By this relationship definition, we know we have a
 one-to-many relationship*/
 
 User.hasMany(Post, {
-    foreignKey: 'userId',
+    foreignKey: 'user_id',
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'userId',
+    foreignKey: 'user_id',
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'userId',
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'userId',
+    foreignKey: 'post_id',
 });
 
 
