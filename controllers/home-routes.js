f496c883eba1c40a41a66313b47b83546f4926c5
Devmadia <agathaphan@icloud.com>
2020-08-24 20:53:56 -0500

    included login and signup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,4 +38,25 @@ router.get('/', (req, res) => {
     });
 });
 
+// login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+
+    res.render('login');
+});
+
+// sign up
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+
+    res.render('signup');
+});
+
+
 module.exports = router;
